a3a68441d10fa97ff5cc8de0e542615a15f907ca
anh <hotrunganhht1912@gmail.com>
2020-12-03 13:15:50 +0700

    rapporteur update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
+import Rapporteur from "../components/rapporteur"
 import SEO from "../components/seo"
 import Organizer from '../components/organizer'
 import Donor from '../components/donor'
@@ -10,6 +11,7 @@ const IndexPage = () => (
     <SEO title="CME" />
     <Header />
     <Organizer />
+    <Rapporteur/>
     <Donor />
   </Layout>
 )
