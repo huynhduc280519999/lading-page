598d83e25ce53de4ef465b121c5215d07a44eff3
anh <hotrunganhht1912@gmail.com>
2020-12-03 18:19:59 +0700

    add foter

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,18 @@ import Header from "../components/header"
 import Layout from "../components/layout"
 import Rapporteur from "../components/rapporteur"
 import SEO from "../components/seo"
-import Organizer from '../components/organizer'
-import Donor from '../components/donor'
+import Organizer from "../components/organizer"
+import Donor from "../components/donor"
+import Footer from "../components/footer"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="CME" />
     <Header />
     <Organizer />
-    <Rapporteur/>
+    <Rapporteur />
     <Donor />
+    <Footer />
   </Layout>
 )
 
