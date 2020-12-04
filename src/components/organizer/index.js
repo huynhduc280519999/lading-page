ae458c07911ddf1504e9cab17d0b1b7b67d5066b
anh <hotrunganhht1912@gmail.com>
2020-12-04 18:42:27 +0700

    ok

--- src/components/organizer/index.js
+++ src/components/organizer/index.js
@@ -1,56 +1,75 @@
-import React from 'react'
-import Person from './person'
-import './index.css'
+import React from "react"
+import Person from "./person"
+import "./index.css"
+import Bs1 from "../../images/bs1.png"
+import Bs2 from "../../images/bs2.png"
+import Bs3 from "../../images/bs3.png"
+import Bs4 from "../../images/bs4.png"
+import Bs5 from "../../images/bs5.png"
+import Bs6 from "../../images/bs6.png"
+import Bs7 from "../../images/bs7.png"
+import Bs8 from "../../images/bs8.png"
+
 
 const Organizer = () => {
   const persons = [
     {
-      name: 'võ tấn sơn',
-      title: 'PGS. TS',
-      position: 'Chủ tịch Hội PTTK Việt Nam'
+      name: "võ tấn sơn",
+      title: "PGS. TS",
+      position: "Chủ tịch Hội PTTK Việt Nam",
+      avatar: Bs1,
     },
     {
-      name: 'lê văn tiến',
-      title: 'BS. CKII',
-      position: 'Giám Đốc BVĐK Lâm Đồng'
+      name: "lê văn tiến",
+      title: "BS. CKII",
+      position: "Giám Đốc BVĐK Lâm Đồng",
+      avatar: Bs2,
     },
     {
-      name: 'nguyễn phong',
-      title: 'TS. BS',
-      position: 'Phó Chủ tịch Hội PTTK Việt Nam'
+      name: "nguyễn phong",
+      title: "TS. BS",
+      position: "Phó Chủ tịch Hội PTTK Việt Nam",
+      avatar: Bs3,
     },
     {
-      name: 'đồng văn hệ',
-      title: 'PGS. TS',
-      position: 'Phó Chủ tịch Hội PTTK Việt Nam'
+      name: "đồng văn hệ",
+      title: "PGS. TS",
+      position: "Phó Chủ tịch Hội PTTK Việt Nam",
+      avatar: Bs4,
     },
     {
-      name: 'nguyễn trọng thiện',
-      title: 'TS. BS',
-      position: 'Phó Chủ tịch Hội PTTK Việt Nam'
+      name: "nguyễn trọng thiện",
+      title: "TS. BS",
+      position: "Phó Chủ tịch Hội PTTK Việt Nam",
+      avatar: Bs5,
     },
     {
-      name: 'nguyễn minh anh',
-      title: 'TS. BS',
-      position: 'Tổng thư ký Hội PTTK Việt Nam'
+      name: "nguyễn minh anh",
+      title: "TS. BS",
+      position: "Tổng thư ký Hội PTTK Việt Nam",
+      avatar: Bs6,
     },
     {
-      name: 'nguyễn minh thu',
-      title: 'BS. CKII',
-      position: 'Trưởng khoa Ngoại Thần Kinh BVĐK Lâm Đồng'
+      name: "nguyễn minh thu",
+      title: "BS. CKII",
+      position: "Trưởng khoa Ngoại Thần Kinh BVĐK Lâm Đồng",
+      avatar: Bs7,
     },
     {
-      name: 'phạm vũ thanh',
-      title: 'BS. CKII',
-      position: 'Trưởng phòng Kế Hoạch Tổng Hợp BVĐK Lâm Đồng'
-    }
+      name: "phạm vũ thanh",
+      title: "BS. CKII",
+      position: "Trưởng phòng Kế Hoạch Tổng Hợp BVĐK Lâm Đồng",
+      avatar: Bs8,
+    },
   ]
   return (
     <div className="organizer">
       <h1 className="organizer__title">Ban tổ chức</h1>
       <div className="dash"></div>
       <div className="organizer__content">
-        {persons.map(person => <Person {...person} />)}
+        {persons.map(person => (
+          <Person {...person} />
+        ))}
       </div>
     </div>
   )
